2786fe851aedf274b940e8ce8c65cdd022b7fb09
ParthArora <partharora.india@gmail.com>
2021-01-08 23:56:47 +0530

    Footer added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/font-awesome/css/font-awesome.min.css'
 import Login from './components/Login';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="">
       <Header />
       <Login />
-      
+      <Footer />
     </div>
   );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,13 @@
+
+
 const Footer = () => {
-  
+  return(
+    <div className='footer'>
+      <p>
+        Made with <i className='fa fa-heart'></i> by Parth Arora and Pulkit Aggarwal
+      </p>
+    </div>
+  )
 }
+
+export default Footer;
